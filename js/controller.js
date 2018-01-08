67558fe23571442347a645e646b16dc226d45424
Laura Innis <laura.innis@outlook.com>
2018-01-08 10:31:48 +0000

    bower install, init, install bootstrap, angular, angular-route since bower-components were removed for gh-pages, and also fixed typo in controller name

--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,5 @@
  
-var myApp= angular.module('closeprotectApp', [])
+var myApp= angular.module('closeprotectAPP', [])
 	app.controller('MainCtrl', ['$scope', function($scope){
 	$scope.hello = 'world123';
 
